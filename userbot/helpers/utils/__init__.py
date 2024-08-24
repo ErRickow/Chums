f7c38ebd6ca037a7abdd2d63c07051ad7265daef
Just R <remixingmagelang@gmail.com>
2024-08-24 10:38:34 +0700

    update userbot/helpers/utils/__init__.py

--- userbot/helpers/utils/__init__.py
+++ userbot/helpers/utils/__init__.py
@@ -13,8 +13,8 @@ check = 0
 while flag:
     try:
         from . import format as _format
-        from . import tools as _pandatools
-        from . import utils as _pandautils
+        from . import tools as _chumstools
+        from . import utils as _chumsutils
         from .events import *
         from .format import htmlmentionuser, mentionuser, parse_pre
 
