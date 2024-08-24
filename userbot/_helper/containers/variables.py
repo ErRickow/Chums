dc843c29b00155a9a2e95b7da50a4f07576a5148
Just R <remixingmagelang@gmail.com>
2024-08-24 09:20:44 +0700

    create userbot/_helper/containers/variables.py

---
+++ userbot/_helper/containers/variables.py
@@ -0,0 +1,20 @@
+# Copyright (C) 2021 PandaUserbot <https://github.com/ilhammansiz/PandaX_Userbot>
+# maintaince 2023 pyrogram & telethon
+# jangan di hapus ga semuanya dihapus lu paham 😏
+# Pembaruan 2023 skala besar dengan menggabungkan 2 basis telethon and pyrogram.
+# Dibuat dari berbagai userbot yang pernah ada.
+
+class Variables(object):
+    # lower
+    message_ids = {}
+
+    # assistant
+    assistant_name = "Panda Userbot"
+    assistant_age = "20"
+    assistant_gender = "male"
+
+    # userbot
+    userbot_name = "Panda Userbot"
+
+    # photo
+    PIC = "https://telegra.ph/file/c0ebbb1f92c7ef5c8c292.jpg"
