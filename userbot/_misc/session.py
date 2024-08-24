1483000cd46a47cc29fce3fdd3c29b29e46dd893
Just R <remixingmagelang@gmail.com>
2024-08-24 09:30:25 +0700

    create userbot/_misc/session.py

---
+++ userbot/_misc/session.py
@@ -0,0 +1,15 @@
+# Copyright (C) 2021 PandaUserbot <https://github.com/ilhammansiz/PandaX_Userbot>
+# maintaince 2023 pyrogram & telethon
+# jangan di hapus ga semuanya dihapus lu paham 😏
+# Pembaruan 2023 skala besar dengan menggabungkan 2 basis telethon and pyrogram.
+# Dibuat dari berbagai userbot yang pernah ada.
+# t.me/pandac0de t.me/pandauserbot
+
+
+from ..Session import *
+Bot = PandaBot
+Stark = PandaBot
+tgbot = tgbot
+pandaub = PandaBot
+PandaBot2 =  PandaBot2
+PandaBot3 = PandaBot3
